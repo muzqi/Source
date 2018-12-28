8ee5196942b5bb4080b315f6dd91bbc9e3513228
liqi <liqi1@cmiot.chinamobile.com>
2018-12-28 15:36:32 +0800

    feat: 全新 electron-react 脚手架初版

---
+++ Cli/electron-react/README.md
@@ -0,0 +1,33 @@
+## 开始
+
+### 实现
+- 使用 electron + create-react-app + antd 封装而成的跨平台桌面端脚手架
+- 本机系统环境
+  - macOS 10.13.1
+  - nodejs 10.8.0
+  - yarn 1.12.3 [registry: https://registry.npm.taobao.org]
+
+### 构建提供的功能(Webpack)
+- antd, 自助主题配置
+- 全局 LESS 变量配置
+- 支持 ES7 装饰器语法
+- 配置别名 ALIAS
+
+## 启动
+``` bash
+yarn run start
+```
+
+## 打包构建
+
+### mac
+
+``` bash
+yarn run build:mac
+```
+
+### win
+
+``` bash
+yarn run build:win
+```
---
+++ Cli/electron-react/config-overrides.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+function resolve(dir) {
+  return path.join(__dirname, '.', dir);
+}
+
+module.exports = function override(config, env) {
+  // 生产环境输出路径使用相对路径
+  if (env === 'production') {
+    config.output.publicPath = './';
+  }
+
+  // antd 支持按需加载
+  config = injectBabelPlugin([
+    'import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+    }],
+    config,
+  );
+
+  // 配置 antd 主题, 以及全局 less 颜色变量
+  // 所有样式表:
+  // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+  config = rewireLess.withLoaderOptions({
+    modifyVars: {
+      "@primary-color": "#1DA57A"
+    },
+    javascriptEnabled: true,
+  })(config, env);
+
+  // 支持装饰器语法
+  config = injectBabelPlugin([
+    '@babel/plugin-proposal-decorators', {
+      legacy: true,
+    }], config);
+
+  // 配置别名
+  config.resolve.alias = {
+    '@': resolve('src'),
+  }
+
+  return config;
+};

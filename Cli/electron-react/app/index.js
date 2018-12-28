da269cd78293c1480c74e10556f87dc1826df87d
liqi <liqi1@cmiot.chinamobile.com>
2018-12-28 20:43:07 +0800

    style: 统一文件格式

--- Cli/electron-react/app/index.js
+++ Cli/electron-react/app/index.js
@@ -1,9 +1,9 @@
-const path = require('path');
-const isDev = require('electron-is-dev');
-const { app, BrowserWindow } = require('electron');
+const path = require('path')
+const isDev = require('electron-is-dev')
+const { app, BrowserWindow } = require('electron')
 
 // 利用electron-debug，添加和Chrome类似的快捷键
-isDev && require('electron-debug')({ enabled: true, showDevTools: false });
+isDev && require('electron-debug')({ enabled: true, showDevTools: false })
 
 // 用于添加Chromium插件
 function createDevTools() {
@@ -11,49 +11,49 @@ function createDevTools() {
     default: installExtension,
     REACT_DEVELOPER_TOOLS,
     REDUX_DEVTOOLS,
-  } = require('electron-devtools-installer');
+  } = require('electron-devtools-installer')
   // 安装devtron
-  const devtronExtension = require('devtron');
-  devtronExtension.install();
+  const devtronExtension = require('devtron')
+  devtronExtension.install()
   // 安装React开发者工具
-  installExtension(REACT_DEVELOPER_TOOLS);
-  installExtension(REDUX_DEVTOOLS);
+  installExtension(REACT_DEVELOPER_TOOLS)
+  installExtension(REDUX_DEVTOOLS)
 }
 
-let win;
-const devUrl = 'http://localhost:3000';
+let win
+const devUrl = 'http://localhost:3000'
 // 本地文件路径定位到打包的react文件
-const localUrl = `file://${path.resolve(__dirname, '../../app.asar/build')}/index.html`;
-const appUrl = isDev ? devUrl : localUrl;
+const localUrl = `file://${path.resolve(__dirname, '../../app.asar/build')}/index.html`
+const appUrl = isDev ? devUrl : localUrl
 
 function createWindow() {
   // 创建浏览器窗口。
-  win = new BrowserWindow({ width: 800, height: 600 });
+  win = new BrowserWindow({ width: 800, height: 600 })
 
   // 然后加载应用的 index.html。
-  win.loadURL(appUrl);
+  win.loadURL(appUrl)
 
   // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
-    win = null;
-  });
+    win = null
+  })
 }
 
 app.on('ready', () => {
-  createWindow();
+  createWindow()
   // 只在开发环境加载开发者工具
-  isDev && createDevTools();
-});
+  isDev && createDevTools()
+})
 
 // 当全部窗口关闭时退出。
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
 app.on('activate', () => {
   if (win === null) {
-    createWindow();
+    createWindow()
   }
-});
+})
--- Cli/electron-react/config-overrides.js
+++ Cli/electron-react/config-overrides.js
@@ -1,17 +1,17 @@
-const theme = require('./.theme');
+const theme = require('./.theme')
 
-const path = require('path');
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
+const path = require('path')
+const { injectBabelPlugin } = require('react-app-rewired')
+const rewireLess = require('react-app-rewire-less')
 
 function resolve(dir) {
-  return path.join(__dirname, '.', dir);
+  return path.join(__dirname, '.', dir)
 }
 
 module.exports = function override(config, env) {
   // 生产环境输出路径使用相对路径
   if (env === 'production') {
-    config.output.publicPath = './';
+    config.output.publicPath = './'
   }
 
   // antd 支持按需加载
@@ -22,7 +22,7 @@ module.exports = function override(config, env) {
       style: true,
     }],
     config,
-  );
+  )
 
   // 配置 antd 主题, 以及全局 less 颜色变量
   // 所有样式表:
@@ -30,18 +30,18 @@ module.exports = function override(config, env) {
   config = rewireLess.withLoaderOptions({
     modifyVars: theme,
     javascriptEnabled: true,
-  })(config, env);
+  })(config, env)
 
   // 支持装饰器语法
   config = injectBabelPlugin([
     '@babel/plugin-proposal-decorators', {
       legacy: true,
-    }], config);
+    }], config)
 
   // 配置别名
   config.resolve.alias = {
     '@': resolve('src'),
   }
 
-  return config;
-};
+  return config
+}

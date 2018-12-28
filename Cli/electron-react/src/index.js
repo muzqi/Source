b0b99ac84fb9151f6933c8fd3c686d3d1b88ccd8
liqi <liqi1@cmiot.chinamobile.com>
2018-12-28 15:20:34 +0800

    删除旧的 electron-react

--- Cli/electron-react/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import AppContainer from '@/containers/appContainer.jsx'
-
-ReactDOM.render(
-  <AppContainer />,
-  document.getElementById('root')
-)

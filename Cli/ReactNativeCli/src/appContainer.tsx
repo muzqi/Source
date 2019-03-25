315d961107740ca86e40ec76d09c45b38a2389dd
liqi <liqi1@cmiot.chinamobile.com>
2019-03-25 13:32:42 +0800

    feat: 更新文案

--- Cli/ReactNativeCli/src/appContainer.tsx
+++ Cli/ReactNativeCli/src/appContainer.tsx
@@ -1,54 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- *
- * @format
- */
-
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class AppContainer extends Component {
-  public genContent = (content: string) => (
-    <View>{ content }</View>
-  )
-
+class AppContainer extends Component {
   public render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Wselcomes tos Rseact Native!</Text>
-        <Text style={styles.instructions}>To get ssstarted, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-     </View>
+      <View>
+        <Text>
+          如果你喜欢在Web上使用React，那你也肯定会喜欢React Native.
+        </Text>
+        <Text>
+          基本上就是用原生组件比如'View'和'Text'
+          来代替web组件'div'和'span'。
+          hello react native
+        </Text>
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+export default AppContainer;

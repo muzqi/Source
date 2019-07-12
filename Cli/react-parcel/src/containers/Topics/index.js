5b6deffa1136a7d0e1ca1d1dd4c3c33464bc8e46
liqi <liqi1@cmiot.chinamobile.com>
2019-07-12 17:25:20 +0800

    feat: 完成演示 demo

--- Cli/react-parcel/src/containers/Topics/index.js
+++ Cli/react-parcel/src/containers/Topics/index.js
@@ -2,28 +2,53 @@ import React, { useState, useEffect, useContext } from 'react';
 import { TopicsContext, TopicsProvider } from '../../state/contexts/topics';
 import { topicsReq, topicsSuccess, topicsError } from '../../state/actions/topics';
 
+const fetchTopicsList = ({ page = 0, success = () => { }, fail = () => { } }) => {
+  const url = `https://www.vue-js.com/api/v1/topics?page=${page}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => success(res.data))
+    .catch((err) => fail(err));
+}
+
 const TopicsList = () => {
-  const [isReFetch] = useState(false);
+  const [page, setPage] = useState(1);
   const { state, dispatch } = useContext(TopicsContext);
 
   useEffect(() => {
     dispatch(topicsReq());
-
-    const url = 'https://www.vue-js.com/api/v1/topics';
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => dispatch(topicsSuccess(res.data)))
-      .catch((err) => dispatch(topicsError(err)));
-
-  }, [isReFetch]);
-
-  console.log(state);
+    fetchTopicsList({
+      page,
+      success: (data) => dispatch(topicsSuccess(data)),
+      fail: (err) => dispatch(topicsError(err)),
+    });
+  }, [page]);
 
   return (
     <div>
       <p>{state.isLoading ? 'loading...' : 'done'}</p>
+
+      <div>
+        <p>第{page}页</p>
+        {
+          page !== 1 && (
+            <button onClick={() => setPage(page - 1)}>
+              上一页
+            </button>
+          )
+        }
+        <button onClick={() => setPage(page + 1)}>
+          下一页
+        </button>
+      </div>
+
       <ul>
-        
+        {
+          state.topicsList.length
+            ? state.topicsList.map((d) => (
+              <li key={d.id}>{d.title}</li>
+            ))
+            : null
+        }
       </ul>
     </div>
   )
